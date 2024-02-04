776cb3944402f0c7d1f889b98b906752245fe4aa
Chaimae <chaimae.elmattari@gmail.com>
2024-02-04 15:17:29 +0100

    Exo3 tu veux ma photo

--- src/api.js
+++ src/api.js
@@ -1,5 +1,4 @@
-import fetch from 'node-fetch';
-import crypto from 'crypto';
+import { createHash } from 'crypto';
 
 /**
  * Récupère les données de l'endpoint en utilisant les identifiants
@@ -8,8 +7,8 @@ import crypto from 'crypto';
  * @return {Promise<json>}
  */
 export const getData = async (url) => {
-    // A Compléter exo 3
-    const timestamp = Math.floor(Date.now() / 1000);
+    // exo 3
+    const timestamp = Date.now();
     const publicKey = "507b26042b7eb8fd48ac4281919811e1";
     const privateKey = "f7a9610f5aae980370e8375c5a40f3d4f587af91";
     const hash = await getHash(publicKey, privateKey, timestamp);
@@ -20,16 +19,14 @@ export const getData = async (url) => {
         const response = await fetch(apiUrl);
         const data = await response.json();
 
-        // Effectuer la sélection des résultats qui possèdent une image thumbnail valide
         const resultsWithThumbnail = data.data.results.filter(character =>
             character.thumbnail && character.thumbnail.path !== "image_not_available"
         );
 
-        // Constituer un tableau de Personnage contenant pour le champ imageUrl la destination de la
-        // version portrait_xlarge de l’image.
         const characters = resultsWithThumbnail.map(character => ({
             name: character.name,
             description: character.description,
+            thumbnail: character.thumbnail,
             imageUrl: `${character.thumbnail.path}/portrait_xlarge.${character.thumbnail.extension}`
         }));
 
@@ -49,9 +46,8 @@ export const getData = async (url) => {
  * @return {Promise<ArrayBuffer>} en hexadecimal
  */
 export const getHash = async (publicKey, privateKey, timestamp) => {
-    // A compléter exo 3
-    const hash = crypto.createHash('md5');
+    // exo 3
+    const hash = createHash('md5');
     hash.update(timestamp + privateKey + publicKey);
     return hash.digest('hex');
 }
-
--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log("Je suis Marvel")
+import { getData } from './api.js';  // Assurez-vous que le chemin est correct
+
+// Test avec les personnages
+const apiUrl = 'https://gateway.marvel.com/v1/public/characters?limit=5';
+
+(async () => {
+    try {
+        const charactersData = await getData(apiUrl);
+        console.log('Characters Data:', charactersData);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+})();
